e5eec83bb5b8583ee4b9ffc057ba698bcbbbcfe8
Alex Bryner <abryner@doterra.com>
2020-01-18 15:36:29 -0700

    serve conferences json outside of years

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,37 @@
 var fs = require('fs');
 const path = require('path');
 
+function getJsonFile(filePath, apiRes) {
+  fs.access(filePath, fs.constants.F_OK, filePathErr => {
+    if (filePathErr) {
+      console.error('File Path Error: ', filePathErr);
+    } else
+      fs.readFile(filePath, function(readFilErr, data) {
+        if (readFilErr) {
+          console.error('Read File Error: ', readFilErr);
+        } else {
+          apiRes.json(JSON.parse(data));
+        }
+      });
+  });
+}
+
 // eslint-disable-next-line no-undef
 module.exports = app => {
-  // put your express app logic here
+  app.get('/api/:table', (req, apiRes) => {
+    try {
+      const filePath = path.join('dist/data/', req.params.table) + '.json';
+      getJsonFile(filePath, apiRes);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
   app.get('/api/:year/:table', (req, apiRes) => {
     try {
       const filePath =
         path.join('dist/data/', req.params.year, req.params.table) + '.json';
-
-      fs.access(filePath, fs.constants.F_OK, filePathErr => {
-        if (filePathErr) {
-          console.error('File Path Error: ', filePathErr);
-        } else
-          fs.readFile(filePath, function(readFilErr, data) {
-            if (readFilErr) {
-              console.error('Read File Error: ', readFilErr);
-            } else {
-              apiRes.json(JSON.parse(data));
-            }
-          });
-      });
+      getJsonFile(filePath, apiRes);
     } catch (err) {
       console.error(err);
     }
