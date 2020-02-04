19ff4fcd24df7fbfbaee6eddca32a6609db1b410
AlexBryner <alexbryner@outlook.com>
2020-02-03 20:22:03 -0700

    separating out functions

--- src/client/modules/view/scanner/scanner.js
+++ src/client/modules/view/scanner/scanner.js
@@ -7,78 +7,86 @@ export default class ViewScanner extends LightningElement {
   @track qrCodeResult = '';
   @track haveCameraAccess = false;
 
+  video;
+  canvasElement;
+  canvas;
+
   renderedCallback() {
-    let video = this.template.querySelector('video');
-    let canvasElement = this.template.querySelector('canvas');
-    let canvas = canvasElement.getContext('2d');
+    if (!this.canvas) {
+      this.video = this.template.querySelector('video');
+      this.canvasElement = this.template.querySelector('canvas');
+      this.canvas = this.canvasElement.getContext('2d');
 
-    function drawLine(begin, end, color) {
-      canvas.beginPath();
-      canvas.moveTo(begin.x, begin.y);
-      canvas.lineTo(end.x, end.y);
-      canvas.lineWidth = 4;
-      canvas.strokeStyle = color;
-      canvas.stroke();
-    }
-
-    // Use facingMode: environment to attemt to get the front camera on phones
-    navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: 'environment' } })
-      .then(function(stream) {
-        video.srcObject = stream;
-        video.setAttribute('playsinline', true); // required to tell iOS safari we don't want fullscreen
-        video.play();
-        requestAnimationFrame(tick);
-      });
-
-    function tick() {
-      if (video.readyState === video.HAVE_ENOUGH_DATA) {
-        this.haveCameraAccess = true;
-
-        canvasElement.height = video.videoHeight;
-        canvasElement.width = video.videoWidth;
-        canvas.drawImage(
-          video,
-          0,
-          0,
-          canvasElement.width,
-          canvasElement.height
-        );
-        let imageData = canvas.getImageData(
-          0,
-          0,
-          canvasElement.width,
-          canvasElement.height
-        );
-        let code = jsQR(imageData.data, imageData.width, imageData.height, {
-          inversionAttempts: 'dontInvert'
+      // Use facingMode: environment to attemt to get the front camera on phones
+      navigator.mediaDevices
+        .getUserMedia({ video: { facingMode: 'environment' } })
+        .then(function(stream) {
+          this.video.srcObject = stream;
+          this.video.setAttribute('playsinline', true); // required to tell iOS safari we don't want fullscreen
+          this.video.play();
+          // eslint-disable-next-line @lwc/lwc/no-async-operation
+          requestAnimationFrame(this.tick);
         });
-        if (code) {
-          drawLine(
-            code.location.topLeftCorner,
-            code.location.topRightCorner,
-            '#FF3B58'
-          );
-          drawLine(
-            code.location.topRightCorner,
-            code.location.bottomRightCorner,
-            '#FF3B58'
-          );
-          drawLine(
-            code.location.bottomRightCorner,
-            code.location.bottomLeftCorner,
-            '#FF3B58'
-          );
-          drawLine(
-            code.location.bottomLeftCorner,
-            code.location.topLeftCorner,
-            '#FF3B58'
-          );
-          this.haveQrCode = true;
-          this.qrCodeResult = code.data;
-        }
-      }
-      requestAnimationFrame(tick);
     }
   }
+
+  drawLine = (begin, end, color) => {
+    this.canvas.beginPath();
+    this.canvas.moveTo(begin.x, begin.y);
+    this.canvas.lineTo(end.x, end.y);
+    this.canvas.lineWidth = 4;
+    this.canvas.strokeStyle = color;
+    this.canvas.stroke();
+  };
+
+  tick = () => {
+    if (this.video.readyState === this.video.HAVE_ENOUGH_DATA) {
+      this.haveCameraAccess = true;
+
+      this.canvasElement.height = this.video.videoHeight;
+      this.canvasElement.width = this.video.videoWidth;
+      this.canvas.drawImage(
+        this.video,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      let imageData = this.canvas.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      let code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+      if (code) {
+        this.drawLine(
+          code.location.topLeftCorner,
+          code.location.topRightCorner,
+          '#FF3B58'
+        );
+        this.drawLine(
+          code.location.topRightCorner,
+          code.location.bottomRightCorner,
+          '#FF3B58'
+        );
+        this.drawLine(
+          code.location.bottomRightCorner,
+          code.location.bottomLeftCorner,
+          '#FF3B58'
+        );
+        this.drawLine(
+          code.location.bottomLeftCorner,
+          code.location.topLeftCorner,
+          '#FF3B58'
+        );
+        this.haveQrCode = true;
+        this.qrCodeResult = code.data;
+      }
+    }
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    requestAnimationFrame(this.tick);
+  };
 }
